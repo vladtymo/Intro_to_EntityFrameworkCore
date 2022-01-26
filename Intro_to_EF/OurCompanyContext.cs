f2bf0622cd70f7a68681103c579a614c6222a1a2
vladtymo <tymo.vlad@gmail.com>
2022-01-26 16:28:15 +0200

    Create Many to Many Relationship.

--- Intro_to_EF/OurCompanyContext.cs
+++ Intro_to_EF/OurCompanyContext.cs
@@ -12,23 +12,36 @@ namespace Intro_to_EF
 {
     public class OurCompanyContext : DbContext
     {
-        public OurCompanyContext() 
+        public OurCompanyContext()
         {
+            Database.EnsureDeleted();
             Database.EnsureCreated();
         }
+        public OurCompanyContext(DbContextOptions options) : base(options) { }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            base.OnConfiguring(optionsBuilder);
-
-            optionsBuilder.UseSqlServer(
+            optionsBuilder.UseSqlServer(//@"Data Source=DESKTOP-O0M8V28\SQLEXPRESS;Initial Catalog=OurCompany;Integrated Security=True;Connect Timeout=3");
                 ConfigurationManager.ConnectionStrings["CompanyConnStr"].ConnectionString);
+
+            base.OnConfiguring(optionsBuilder);
         }
 
-        // Collections => Tables
+        // ---------- COLLECTIONS (Tables in DB) ----------
         public DbSet<Worker> Workers { get; set; }
         public DbSet<Department> Departments { get; set; }
         public DbSet<Country> Countries { get; set; }
+        public DbSet<Project> Projects { get; set; }
+    }
+
+    public class Project
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime LaunchDate { get; set; }
+
+        // ---------- NAVIGATION PROPERTIES ----------
+        public ICollection<Worker> Workers { get; set; }
     }
 
     public class Country
@@ -37,7 +50,7 @@ namespace Intro_to_EF
         [Required, MaxLength(250)]          // set as nvarchar(250)
         public string Name { get; set; }
 
-        // Navigation Properties
+        // ---------- NAVIGATION PROPERTIES ----------
         public ICollection<Worker> Workers { get; set; }
     }
 
@@ -47,7 +60,7 @@ namespace Intro_to_EF
         public int Number { get; set; }
         public string PhoneNumber { get; set; }
 
-        // Navigation Properties
+        // ---------- NAVIGATION PROPERTIES ----------
         public ICollection<Worker> Workers { get; set; }
     }
 
@@ -55,23 +68,38 @@ namespace Intro_to_EF
     [Table("Employees")]                    // set custom table name in db
     public class Worker
     {
-        // Properties => Columns
+        public Worker()
+        {
+            Projects = new HashSet<Project>();
+        }
+        // ---------- PROPERTIES (Columns in DB) ----------
 
         // Primary key: Id, EntityName+Id (WorkerId)
         public int Id { get; set; }
 
         [Column("FirstName")]               // set custom column name in db
         [Required]                          // set as not null type in db
+        [MaxLength(120)]                    // set maximum length
         public string Name { get; set; }
 
-        [Column("LastName"), Required]
+        [Column("LastName"), Required, MaxLength(120)]
         public string Surname { get; set; }
 
         public decimal Salary { get; set; }
         public DateTime? Birthdate { get; set; }
 
-        // Navigation Properties
+        [MaxLength(200)]
+        public string Address { get; set; }
+
+        // ---------- NAVIGATION PROPERTIES ----------
+        // Relationship type: Zero or One to Many (0/1...*)
         public Country Country { get; set; }
+
+        // Relationship type: One to Many (1...*)
+        [Required]
         public Department Department { get; set; }
+
+        // Relationship type: Many to Many (*...*)
+        public ICollection<Project> Projects { get; set; }
     }
 }
--- Intro_to_EF/Program.cs
+++ Intro_to_EF/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Configuration;
+using System.Linq;
 
 namespace Intro_to_EF
 {
@@ -10,11 +12,36 @@ namespace Intro_to_EF
 
             OurCompanyContext context = new OurCompanyContext();
 
-            //context.Countries.Add(new Country() { Name = "Ukraine" });
-            //context.Countries.Add(new Country() { Name = "Italy" });
-            //context.Countries.Add(new Country() { Name = "Spain" });
+            context.Countries.Add(new Country() { Name = "Ukraine" });
+            context.Countries.Add(new Country() { Name = "Italy" });
+            context.Countries.Add(new Country() { Name = "Spain" });
 
-            //context.SaveChanges();
+            var dep1 = context.Departments.Add(new Department() { PhoneNumber = "54-55-66" });
+            var dep2 = context.Departments.Add(new Department() { PhoneNumber = "76-66-66" });
+
+            context.Projects.Add(new Project() { Name = "Project X", LaunchDate = DateTime.Now });
+            context.Projects.Add(new Project() { Name = "Kobra", LaunchDate = new DateTime(2012, 12, 12) });
+
+            context.SaveChanges();
+
+            Country country = context.Countries.First();
+            Department dep = context.Departments.First();
+            Project proj = context.Projects.First();
+
+            Worker worker = new Worker()
+            {
+                Name = "Igor",
+                Surname = "Shan",
+                Address = "Lublinska 43",
+                Birthdate = new DateTime(1994, 4, 12),
+                Salary = 3600,
+                Country = country,
+                Department = dep
+            };
+            worker.Projects.Add(proj);
+            context.Workers.Add(worker);
+
+            context.SaveChanges();
 
             foreach (var c in context.Countries)
             {
